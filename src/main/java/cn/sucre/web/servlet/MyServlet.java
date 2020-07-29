7f8276494ec68b1afd82b94f9d97de58b4b8d561
tangtangsama <1462339680@qq.com>
2020-07-29 16:25:55 +0800

    Initial commit

---
+++ src/main/java/cn/sucre/web/servlet/MyServlet.java
@@ -0,0 +1,15 @@
+package cn.sucre.web.servlet;
+
+import java.io.IOException;
+
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sucre
+  Date: 2020/7/28
+  Time: 15:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+        hello maven
+    </body>
+</html>
