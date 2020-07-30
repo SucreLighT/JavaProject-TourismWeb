f73d6a4e4ce7e7e85c92832434ca1f16627d6594
tangtangsama <1462339680@qq.com>
2020-07-30 16:19:54 +0800

    设置编码的过滤器

---
+++ src/main/java/cn/sucre/web/filter/CharacterFilter.java
@@ -0,0 +1,42 @@
+package cn.sucre.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description: 过滤器 用于设置编码格式
+ * @author: sucre
+ * @date: 2020/07/30
+ * @time: 14:25
+ */
+@WebFilter(urlPatterns = "/*")
+public class CharacterFilter implements Filter {
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        //将父接口转为子接口
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        //获取请求方法
+        String method = request.getMethod();
+        //解决post请求中文数据乱码问题
+        if (method.equalsIgnoreCase("post")) {
+            request.setCharacterEncoding("utf-8");
+        }
+        //处理响应乱码
+        response.setContentType("text/html;charset=utf-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
