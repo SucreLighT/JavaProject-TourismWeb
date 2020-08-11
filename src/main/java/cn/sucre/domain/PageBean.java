d96cda64b1c1116c6663d2d142972dbe44a7541a
tangtangsama <1462339680@qq.com>
2020-08-11 16:30:26 +0800

    添加实体类：分页对象

---
+++ src/main/java/cn/sucre/domain/PageBean.java
@@ -0,0 +1,58 @@
+package cn.sucre.domain;
+
+import java.util.List;
+
+/**
+ * @description: 分页对象
+ * @author: sucre
+ * @date: 2020/08/11
+ * @time: 16:22
+ */
+public class PageBean<T> {
+    private int totalCount;
+    private int totalPage;
+    private int currentPage;
+    private int pageSize;
+    private List<T> list;
+
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
